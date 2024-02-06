d932c107df8696e332afa4186bca3b6d9049c604
Riachuelo <riachuelo@riachuelos-mbp.lan>
2024-02-05 19:56:02 -0800

    nc

--- test_print_and_log.sh
+++ test_print_and_log.sh
@@ -2,10 +2,10 @@
 
 ## Basic functionality of logging the date and a message everytime that a scheduled job is run
 # Print date and time
-message="--> Does this work?"
+message="Does this work?"
 current_date=$(date)
 echo "$current_date $message"
-record=($current_date $message)
+result="$current_date $message"
 
 # Append values to log
-echo $record >> print_message.log
+echo "$result" >> print_message.log
